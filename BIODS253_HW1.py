617c8577569b4793e79b71487339ce8d1f000b08
drendon28 <drendon2882@gmail.com>
2024-01-21 11:56:33 -0800

    graph triangle vs runtime

--- BIODS253_HW1.py
+++ BIODS253_HW1.py
@@ -1,12 +1,26 @@
+import matplotlib.pyplot as plt
 from math import factorial
-
-lines = int(input("Enter the number of lines"))
-for row in range(lines):
-    for column in range(lines-row+1):
+from datetime import datetime
+ 
+n = int(input('How many lines?'))
+ 
+linesX = []
+timeY = []
+starttime = datetime.now()
+ 
+for i in range(n):
+    for j in range(n-i+1):
         print(end=" ")
-
-    for column in range(row+1):
-        # nCr = n!/(n-r)!*r
-        print(factorial(row)//(factorial(column)*factorial(row-column)), end=" ")
-
+    for j in range(i+1):
+        # nCr = n!/((n-r)!*r!)
+        print(factorial(i)//(factorial(j)*factorial(i-j)), end=" ")
     print()
+    linesX.append(i + 1)
+    timeY.append((datetime.now() - starttime).total_seconds() *1000) # Time delta in milliseconds
+ 
+ 
+plt.scatter(linesX, timeY)
+plt.ylabel('Time (ms)')
+plt.xlabel('Row count')
+plt.title('Pascal\'s Triangle Size Versus Runtime')
+plt.show()
